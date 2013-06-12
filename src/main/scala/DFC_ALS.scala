7c18c1123aa68f7d5e3fe2028bef859a8f944845
Ginger Smith <vsmith@berkeley.edu>
2013-06-12 13:49:28 -0700

    adding print statement

--- src/main/scala/DFC_ALS.scala
+++ src/main/scala/DFC_ALS.scala
@@ -113,7 +113,7 @@ object DFC_ALS{
     println("mapped to array")
     val columnRdd = sc.parallelize(M_array, nsplits)
     println("parallelized array")
-    println(columnRDD.count)
+    println(columnRdd.count)
     val acc2 = columnRdd.mapPartitionsWithSplit{ case(i, columns) =>
       println("column to array")
       val colArray = columns.toArray
