4f4f4ef8e35ed8c02b52964a8f70d09a1be60513
Ginger Smith <vsmith@berkeley.edu>
2013-06-13 11:28:18 -0700

    adding replicated functionality

--- src/main/scala/Blocked_Join_ALS.scala
+++ src/main/scala/Blocked_Join_ALS.scala
@@ -318,7 +318,7 @@ object Blocked_Join_ALS {
     else { 
       if(big){
         trainData = sc.textFile(trainfile)
-        .map(x => x.subSequence(1,x.length-1))
+        .map(x => x.subSequence(1,x.length-1).toString)
         .map(_.split(','))
         .map{elements => (elements(0).toInt-1,elements(1).toInt-1,elements(2).toDouble)}.cache
       }
