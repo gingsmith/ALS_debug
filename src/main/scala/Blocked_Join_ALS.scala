68273a89b67d25c18f1413916f3c20e0edd3edcf
Ginger Smith <vsmith@berkeley.edu>
2013-06-13 10:47:04 -0700

    adding replicated functionality

--- src/main/scala/Blocked_Join_ALS.scala
+++ src/main/scala/Blocked_Join_ALS.scala
@@ -305,13 +305,13 @@ object Blocked_Join_ALS {
     trainData = sc.textFile(trainfile,nsplits)
       .map(_.split(' '))
       .map{ elements => (elements(0).toInt-1,elements(1).toInt-1,elements(2).toDouble)}
-
-      .map(elements => (c, (r,v))).groupByKey()
       .flatMap( x => replicate(x,repfact,m,n) ).saveAsTextFile(trainfile+"_replicated")
       System.exit(0)
       //.cache
       //Array(x,(x._1+m,x._2,x._3),(x._1,x._2+n,x._3),(x._1+m,x._2+n,x._3)))
       //.persist(StorageLevel.MEMORY_ONLY_SER)
+
+      //.map(elements => (c, (r,v))).groupByKey()
     }
     else { 
       if(big){
