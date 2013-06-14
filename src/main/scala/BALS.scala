a6708d79703d8242b8a4a13489523d1c59dd2cd5
Ginger Smith <vsmith@berkeley.edu>
2013-06-14 12:10:34 -0700

    trying cache instead

--- src/main/scala/BALS.scala
+++ src/main/scala/BALS.scala
@@ -88,13 +88,13 @@ object Broadcast_ALS {
         trainData = sc.textFile(trainfile)
         .map(_.split(' '))
         .map{elements => (elements(0).toInt,elements(1).toInt,elements(2).toDouble)}
-        .persist(StorageLevel.MEMORY_ONLY_SER)
+        .cache//.persist(StorageLevel.MEMORY_ONLY_SER)
       }
       else {
       trainData = sc.textFile(trainfile, nsplits)
         .map(_.split(' '))
         .map{elements => (elements(0).toInt-1,elements(1).toInt-1,elements(2).toDouble)}
-        .persist(StorageLevel.MEMORY_ONLY_SER)
+        .cache//.persist(StorageLevel.MEMORY_ONLY_SER)
       }
     }
 
