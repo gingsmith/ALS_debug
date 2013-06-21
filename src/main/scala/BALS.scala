bb6f4caf3080d81ef2e949f39adc6b60674fdaab
Ginger Smith <vsmith@berkeley.edu>
2013-06-20 18:09:40 -0700

    cleaning up broadcast_als

--- src/main/scala/BALS.scala
+++ src/main/scala/BALS.scala
@@ -19,9 +19,6 @@ object Broadcast_ALS {
 
    def main(args: Array[String]){
 
-    // sbt/sbt "run-main dmf.Run_ALS_Spark master data/ eachmovie_randSplit1_data.txt eachmovie_randSplit1_testData.txt 1648 30000 10 .01 10 .001 /root/spark jarfile src/main/scala/results/ eachmovie_ALS_spark_results.txt"
-    // sbt/sbt "run-main dmf.Run_stripped_ALS_Spark master data/ netflix_randSplit1_data.txt netflix_randSplit1_testData.txt 17770 480189 10 .01 10 sparkhome jar src/main/scala/results/ netflix_ALS_spark_results.txt"
-
     System.setProperty("spark.serializer", "spark.KryoSerializer")
     System.setProperty("spark.kryo.registrator", "als_debug.CCDKryoRegistrator")
     System.setProperty("spark.storage.blockManagerHeartBeatMs", "120000")
@@ -44,11 +41,8 @@ object Broadcast_ALS {
     val jar = options.getOrElse("jars", "")
     val nsplits = options.getOrElse("nsplits", "4").toInt
     val sparkhome = options.getOrElse("sparkhome", System.getenv("SPARK_HOME"))
-    val big = options.getOrElse("big","false").toBoolean
-    val first = options.getOrElse("first","false").toBoolean
     val m = options.getOrElse("m", "100").toInt
     val n = options.getOrElse("n", "100").toInt
-    val repfact = options.getOrElse("repfact", "1").toInt
 
     // print out input
     println("master:       " + master)
@@ -60,54 +54,25 @@ object Broadcast_ALS {
     println("jar:          " + jar)
     println("sparkhome:    " + sparkhome)
     println("nsplits:      " + nsplits)  
-    println("big:          " + big) 
-    println("first time    " + first) 
     println("m:            " + m)
     println("n:            " + n)
-    println("repfact:          " + repfact)  
 
     // set up spark context ..
-    // local[x] runs x threads
     val sc = new SparkContext(master,"BALS",sparkhome,List(jar))
 
     var trainData: spark.RDD[(Int,Int,Double)] = null
 
-    // if(big && first){
-    //   val newfile = trainfile + "_replicated"
-    //   trainData = sc.textFile(trainfile,nsplits)
-    //     .map(_.split(' '))
-    //     .map{ elements => (elements(0).toInt-1,elements(1).toInt-1,elements(2).toDouble)}
-    //     .flatMap(x => replicate(x,repfact,m,n) )
-
-    //   trainData.map(x => (x._1 + " " + x._2 + " " + x._3)).saveAsTextFile(newfile)
-    //   sc.stop()
-    //   System.exit(0)
-    // }
-    // else { 
-      if(big){
-        trainData = sc.textFile(trainfile,nsplits)
-        .coalesce(nsplits)
-        .map(_.split(' '))
-        .map{elements => (elements(0).toInt,elements(1).toInt,elements(2).toDouble)}
-        //.cache
-        .persist(StorageLevel.MEMORY_ONLY_SER)
-      }
-      else {
-      trainData = sc.textFile(trainfile, nsplits)
-        .map(_.split(' '))
-        .map{elements => (elements(0).toInt-1,elements(1).toInt-1,elements(2).toDouble)}
-        .cache//.persist(StorageLevel.MEMORY_ONLY_SER)
-      }
+    // read in training data
+    trainData = sc.textFile(trainfile,nsplits)
+      .coalesce(nsplits)
+      .map(_.split(' '))
+      .map{elements => (elements(0).toInt,elements(1).toInt,elements(2).toDouble)}
+      .persist(StorageLevel.MEMORY_ONLY_SER)
 
     println("num partitions is: " + trainData.partitions.size)
 
-    // initialize W,H
-    //val rand = new Random(seed)
     val W_array = Array.fill(m)(DoubleMatrix.rand(rank));
     val H_array = Array.fill(n)(DoubleMatrix.rand(rank));
-    //val W = DenseMatrix.zeros[Double](m,rank);
-    //val r = new Random(0)
-    //val H = DenseMatrix.rand(n,rank,r);
 
     val WH = ALS(W_array,H_array,sc,trainData,m,n,rank,lambda,niter)
     
@@ -119,11 +84,7 @@ object Broadcast_ALS {
        : (Array[DoubleMatrix],Array[DoubleMatrix]) = {
 
     val lambI = DoubleMatrix.eye(k).mul(lambda);
-    //val lambI = DenseMatrix.eye[Double](k) :*= lambda;
 
-    // arrays of dense vectors
-    //var W_array = (0 to m-1).map{i => new DenseVector(W(i,::).iterator.map{case(k,v)=>v}.toArray)}.toArray
-    //var H_array = (0 to n-1).map{i => new DenseVector(H(i,::).iterator.map{case(k,v)=>v}.toArray)}.toArray
     var W_b = sc.broadcast(W_array)
     var H_b = sc.broadcast(H_array)
 
@@ -133,7 +94,6 @@ object Broadcast_ALS {
     println("MAX COL: " + train_ratings.map{case (w,h,r)=>h}.reduce(Math.max))
     println("NNZ : " + train_ratings.count())
     val train_nnz = train_ratings.count()
-    //val test_nnz = test_ratings.count()
 
     val starttime = System.currentTimeMillis
 
@@ -175,16 +135,5 @@ object Broadcast_ALS {
 
     return (W_array,H_array)
    }
-
-   def replicate(x: (Int,Int,Double), repfact: Int, m:Int, n:Int): Array[(Int,Int,Double)] = {
-    val ret_arr = new Array[(Int,Int,Double)](repfact*repfact)
-    for(i<-0 until repfact){
-      for(j<-0 until repfact){
-        val ind = i*repfact+j
-        ret_arr(ind) = (x._1+i*m,x._2+j*n,x._3)
-      }    
-    }
-    return ret_arr
-  }
 
 }
